939e0da706d13ee0601acd4400f8f64757be6a6a
Raffael N Ferreira <raffaelnascimentof@gmail.com>
2021-11-07 19:10:51 -0300

    Fix tests

--- src/index.js
+++ src/index.js
@@ -51,10 +51,16 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+  if (!user) return response.status(404).json({ error: "User not found" });
+
+  request.user = user;
+  next();
 }
 
-app.post('/users', checksExistsUserAccount, (request, response) => {
+app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
   const usernameAlreadyExists = users.some((user) => user.username === username);
